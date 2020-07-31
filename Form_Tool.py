706523e6d6c3ee23b299de7f634e7d6015ccf6da
puddlejumper90 <55165215+puddlejumper90@users.noreply.github.com>
2020-07-31 15:10:13 -0500

    Added several new feature
    I am going to try to go ahead and have the application store it's own data in an SQL database that is created at the first start of the application. This will be my first attempt combining and SQL DB with a GUI application.
    
    Any feedback is always appreciated.

--- Form_Tool.py
+++ Form_Tool.py
@@ -1,18 +1,53 @@
 '''
 The purpose of this application is to allow the user to create their own forms that can be used to easily store data in csv files.
+
+The main menu is the center of the overall application. Each of the buttons on the main menu need to be defined as functions so that they can be called when each button on the main menu is clicked on.
+
+I am going to go ahead and integrate sqlite into the application code.
 '''
 
 #Import the needed libraries
 import tkinter
+import os
 import numpy as np
 import pandas as pd
 import pandasql as psql
+import sqlite3 #Should be able to use this to store the data... Will need to react a new database if the database does not currently exist though.
 from tkinter import *
 
-#Defined functions 1
+#Defined functions
+
 def OBL(): #Prints one blank line
     print("")
-    
+
+def db_connection():
+	conn = sqlite3.connect('Form_DB.db')
+
+
+def b1(): #This will allow the user to create a new form when button 1 is pressed.
+	create_new_form = Tk()
+	create_new_form.title("Create a New Form")
+	cnf_desc = Label(create_new_form, text="The application will now walk you through the process of creating a new form")
+	cnf_desc.pack()
+
+def b2(): #Adds data to an existing form
+	add_data = Tk()
+	add_data.title('Add data to existing form')
+	ad_desc = Label(add_data, text="This function is under development")
+
+def b3(): #Allows the user to exit the application
+	quit()
+
+#Opening Process
+open_screen = Tk()
+open_screen.title("Opening Processes")
+if os.path.isfile('Form_DB.db') == TRUE:
+	Label(open_screen, text="Database has already been established")
+else:
+	db_connection()
+	Label(open_screen, text="Your database has been connected")
+
+
 #Main menu
 m_screen = Tk()
 m_screen.title("Form Tool Main Menu")
@@ -20,12 +55,12 @@ ms_description = Label(m_screen, text="The purpose of this application is to all
 ms_description.pack()
 
 #Main menu buttons
-button1 = Button(m_screen, text="Create New")
+button1 = Button(m_screen, text="Create New", command = b1)
 button1.pack()
 
-button2 = Button(m_screen, text="Add Data To Existing Form")
+button2 = Button(m_screen, text="Add Data To Existing Form", command = b2)
 button2.pack()
 
-button3 = Button(m_screen, text="Cancel")
+button3 = Button(m_screen, text="Cancel", command = b3)
 button3.pack()
 m_screen.mainloop()
